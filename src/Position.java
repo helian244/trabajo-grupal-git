f500102d003d2f58bd6af0d1e0934664478502cc
rogericsr <rogerictober@gmail.com>
2026-05-02 19:14:52 -0400

    feat: add equals() and hashCode() for Position comparison

--- src/Position.java
+++ src/Position.java
@@ -1,5 +1,7 @@
 
 
+import java.util.Objects;
+
 public class Position {
     public final int row;
     public final int col;
@@ -11,6 +13,19 @@ public class Position {
 
     public boolean isValid() {
         return row >= 0 && row < 6 && col >= 0 && col < 6;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        Position pos = (Position) o;
+        return row == pos.row && col == pos.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 
 }
